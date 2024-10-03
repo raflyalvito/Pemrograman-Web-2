49496856ca10cfa31f650b139cd61ee89f875b31
raflyalvito <raflyalvito@gmail.com>
2024-10-03 11:56:51 +0700

    menambahkan file

---
+++ latihan1b.php
@@ -0,0 +1,14 @@
+<?php
+
+$angka = 10;
+
+echo "aku adalah angka $angka </br>";
+$kali = $angka * 8;
+echo "jika aku dikali 8, jumlahku sekarang $kali,</br>";
+$bagi = $kali / 4;
+echo "jika aku dibagi 4, jumlahku sekarang $bagi,</br>";
+$kurang = $bagi - 6;
+echo"jika aku dikurang 6, jumlahku sekarang $kurang,</br>";
+$tambah = $kurang + 2;
+echo"jika aku ditambah 2, jumlahku sekarang $tambah";
+?>
